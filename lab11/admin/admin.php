aa7d46c60c0acd58f847de5c8c7dc46fa7c2fd98
Sebastian Minkowski <91944596+lemmgator@users.noreply.github.com>
2024-01-18 04:43:28 +0100

    lab11

---
+++ lab11/admin/admin.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+
+<?php
+	error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+?>
+
+<html>
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="Content-Language" content="pl" />
+		<meta name="Author" content="Sebastian Minkowski" />
+		<title>Admin Page</title>
+		<link rel="stylesheet" href="../css/style_admin.css"/>
+	</head>
+	
+	<body>
+		<?php
+
+		session_start();
+		include('../cfg.php');
+		function FormularzLogowania()
+		{
+			$wynik = '
+			<div class="logowanie">
+				<h1 class="naglowek">Panel CMS</h1>
+				<form method="post" name="LoginForm" enctype="multipart/form-data" action="' . $_SERVER['REQUEST_URI'] . '">
+					<table class="">
+						<tr><td class="kog4_t">Login: </td><td><input type="text" name="login" class="" /></td></tr>
+						<tr><td class="log4_t">Hasło: </td><td><input type="password" name="pass" class="" /></td></tr>
+						<tr><td>&nbsp;</td><td><input type="submit" name="x1_submit" class="" value="Zaloguj się" /></td></tr>
+					</table>
+				</form>
+			</div>
+			';
+
+			return $wynik;
+		}
+
+		function ListaPodstron()
+		{
+			global $link;
+			if (!isset($_SESSION['status']) || $_SESSION['status'] == 1) {
+				$query = "SELECT * FROM page_list ORDER BY id ASC";
+				$result = mysqli_query($link, $query);
+				echo '<h1 class="naglowek">Lista podstron</h1><center><table>';
+				if($result){
+					while ($row = mysqli_fetch_array($result)) {
+						echo '<tr><td class="tdid"><b>'.$row['id'] . '<b></td><td class="tdnazwa"><b>'.$row['page_title'].'<b></td><td class="tdedytuj"><a href="admin.php?funkcja=edytuj&id='.$row['id'].'"><b>Edytuj</b></a></td><td><a href="admin.php?funkcja=usun&id='.$row['id'].'"><b>Usuń</b></a></td></tr>';
+					}
+					echo '</table></center><br>';
+				}
+				else{
+					echo "Brak stron";
+				}
+			}
+			if(isset($_GET['funkcja']) && $_GET['funkcja'] == 'usun'){
+				UsunPodstrone();
+			}
+			if(isset($_GET['funkcja']) && $_GET['funkcja'] == 'edytuj'){
+
+				EdytujPodstrone();
+			}
+			DodajNowaPodstrone();
+		}
+		
+		function EdytujPodstrone()
+		{
+			global $link;
+			if (isset($_GET['id'])) 
+			{
+				$id = $_GET['id'];
+			}
+			$query = "SELECT * FROM page_list WHERE id='$id' LIMIT 1";
+			$result = mysqli_query($link ,$query);
+			$row = mysqli_fetch_array($result);
+			echo '
+			<div>
+				<h1 class="naglowek"><b>Edytuj podstronę<b/></h1>
+				<form method="post" name="EditForm" enctype="multipart/form-data" action="' . $_SERVER['REQUEST_URI'] . '">
+					<table>
+						<tr><td class="edit_4t"><b>Tytuł podstrony: <b/><br></td><td><input type="text" name="page_title" size="99" value='.$row['page_title'].' /></td></tr>
+						<tr><td class="edit_4t"><b>Treść podstrony: <b/><br></td><td><textarea rows=20 cols=96 name="page_content"/>'.$row['page_content'].'</textarea></td></tr>
+						<tr><td class="edit_4t"><b>Status podstrony: <b/><br></td><td><input type="checkbox" name="status" checked /></td></tr>
+						<tr><td>&nbsp;</td><td><input type="submit" name="x2_submit" class="edycja" value="Edytuj" /></td></tr>
+					</table>
+				</form>
+			</div>
+			';
+			if (isset($_POST['x2_submit'])&& isset($_GET['id'])) 
+			{
+				$id = $_GET['id'];
+				$tytul = $_POST['page_title'];
+				$tresc = $_POST['page_content'];
+				$status = isset($_POST['status']) ? 1 : 0;
+
+				if (!empty($id)) 
+				{
+					$query = "UPDATE page_list SET page_title = '$tytul', page_content = '$tresc', status = $status WHERE id = $id LIMIT 1";
+
+					$result = mysqli_query($link, $query);
+
+					if ($result) {  
+						echo "<script>window.location.href='admin.php';</script>";
+						exit();
+					} else {
+						echo "<center>Błąd podczas edycji: " . mysqli_error($link)."</center>";
+					}
+				}
+			}
+		}
+
+
+		function DodajNowaPodstrone()
+		{
+			global $link;
+			echo '
+			<div class="">
+				<h1 class="naglowek"><b>Dodaj podstronę<b/></h1>
+				<form method="post" name="AddForm" enctype="multipart/form-data" action="' . $_SERVER['REQUEST_URI'] . '">
+					<table class="dodaj">
+						<tr><td class="add_4t"><b>Tytuł podstrony: <b/></td><td><input type="text" name="page_title_add" size="99"/></td></tr>
+						<tr><td class="add_4t"><b>Treść podstrony: <b/></td><td><textarea rows=20 cols=96 name="page_content_add" /></textarea></td></tr>
+						<tr><td class="add_4t"><b>Status podstrony: <b/></td><td><input type="checkbox" name="status_add" checked /></td></tr>
+						<tr><td>&nbsp;</td><td><input type="submit" name="x3_submit" class="dodaj" value="Dodaj" /></td></tr>
+					</table>
+				</form>
+			</div>
+			';
+			if (isset($_POST['x3_submit'])) 
+			{
+				$tytul = $_POST['page_title_add'];
+				$tresc = $_POST['page_content_add'];
+				$status = isset($_POST['status_add']) ? 1 : 0;
+
+				$query = "INSERT INTO page_list (page_title, page_content, status) VALUES ('$tytul', '$tresc', $status)";
+				$result = mysqli_query($link, $query);
+
+				if ($result) {           
+					echo "<script>window.location.href='admin.php';</script>";
+					exit();
+				} else {
+					echo "<center>Błąd podczas dodawania podstrony: " . mysqli_error($link)."</center>";
+				}
+			}
+		}
+
+
+
+		function UsunPodstrone()
+		{
+			global $link;
+			if (isset($_GET['id'])) 
+			{
+				$id = $_GET['id'];
+
+				$query = "DELETE FROM page_list WHERE id = $id LIMIT 1";
+				$result = mysqli_query($link, $query);
+
+				if ($result) 
+				{         
+					echo "<script>window.location.href='admin.php';</script>";
+					exit();
+				} else {
+					echo "<center>Błąd podczas usuwania podstrony: " . mysqli_error($link)."</center>";
+				}
+			}
+		}
+
+		if (isset($_SESSION['status_logowania']) && $_SESSION['status_logowania'] == 1)
+		{
+			echo '<center><br>Jesteś zalogowany i masz dostęp do metod administracyjnych. <br><br></center>';
+			ListaPodstron();
+			echo '<h2 class="naglowek"><a href="kategorie.php">Manage Categories</a><br></h2>';
+			echo '<h2 class="naglowek"><a href="produkty.php">Manage Products</a><br></h2>';
+			echo '<h2 class="naglowek"><a href="logout.php">Logout</a></h2>';
+		}
+		else
+		{
+			echo FormularzLogowania();
+		}
+
+		if(isset($_POST['login']) && isset($_POST['pass']))
+		{
+			if($_POST['login'] == $login && $_POST['pass'] == $pass)
+			{
+				$_SESSION['status_logowania'] = 1;
+				header("Location: admin.php");
+			}
+			else
+			{
+				echo '<center><br><br><br><br><br>Wprowadzono niepoprawne dane! <br><br>Spróbuj zalogować się ponownie.</center>';
+			}
+		}
+
+		?>
+	</body>
+</html>
---
+++ lab11/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+
+<?php
+	// plik index.php generuje strony dynamicznie z bazy danych
+	error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+?>
+
+<html>
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="Content-Language" content="pl" />
+		<meta name="Author" content="Sebastian Minkowski" />
+		<title>@lemmgator - My hobby</title>
+		<link rel="stylesheet" href="./css/style.css"/>
+	</head>
+
+	<body>
+		<div class="main">
+			<table style="width: 100%">
+				<tr>
+					<td><a href="index.php">Main</td>
+					<td><a href="index.php?idp=breakcore">Breakcore</td>
+					<td><a href="index.php?idp=inspiration">Inspiration</td>
+					<td><a href="index.php?idp=music">My Music</td>
+					<td><a href="index.php?idp=audio">Audio Services</td>
+					<td><a href="index.php?idp=contacts">Contacts</td>
+					<td><a href="index.php?idp=filmy">Videos</td>
+				</tr>
+			</table>
+			<br>
+			<?php
+			
+				// łączenie z bazą danych oraz funkcjonalność szukania ID stron z bazy danych
+				
+				include('cfg.php');
+				include('showpage.php');
+				
+				// odpowiednia strona jest generowana przy podaniu właściwego idp
+				$strona = '';
+				if($_GET['idp'] == '') 
+				{echo PokazPodstrone(1);}
+				if($_GET['idp'] == 'breakcore') 
+				{echo PokazPodstrone(2);}
+				if($_GET['idp'] == 'inspiration') 
+				{echo PokazPodstrone(3);}
+				if($_GET['idp'] == 'music') 
+				{echo PokazPodstrone(4);}
+				if($_GET['idp'] == 'audio') 
+				{echo PokazPodstrone(5);}
+				if($_GET['idp'] == 'contacts') 
+				{echo PokazPodstrone(6);}
+				if($_GET['idp'] == 'filmy') 
+				{echo PokazPodstrone(7);}
+				if($_GET['idp'] == 'js') 
+				{echo PokazPodstrone(8);}
+				if($_GET['idp'] == 'admin') 
+				{echo PokazPodstrone(9);}
+			?>
+			
+			<div class="footer">
+				<a href="index.php?idp=js"><img src="./img/lemm.png" alt="lemm working on tunes"></a><br>
+				<?php
+					// stopka :)
+					$nr_indeksu = '164412';
+					$nrGrupy = 'ISI3';
+					echo 'Autor: Sebastian Minkowski '.$nr_indeksu.' grupa '.$nrGrupy.' <br/><br/>';
+				?>
+			</div>
+			
+		</div>
+	</body>
+</html>
